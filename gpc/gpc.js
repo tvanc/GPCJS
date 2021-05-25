0362fd28dae892617855be96502db59ee3779175
Travis Van Couvering <travis@tvanc.com>
2021-05-24 21:01:19 -0700

    Remove more methods

--- gpc/gpc.js
+++ gpc/gpc.js
@@ -24,7 +24,7 @@ import Rectangle from "./geometry/Rectangle.js"
 import ScanBeamTree from "./geometry/ScanBeamTree.js"
 import ScanBeamTreeEntries from "./geometry/ScanBeamTreeEntries.js"
 import StNode from "./geometry/StNode.js"
-// import TopPolygonNode from "./geometry/TopPolygonNode.js"
+import TopPolygonNode from "./geometry/TopPolygonNode.js"
 import VertexNode from "./geometry/VertexNode.js"
 import VertexType from "./geometry/VertexType.js"
 import WeilerAtherton from "./geometry/WeilerAtherton.js"
@@ -60,7 +60,7 @@ export const gpcas = {
     ScanBeamTree,
     ScanBeamTreeEntries,
     StNode,
-    // TopPolygonNode,
+    TopPolygonNode,
     VertexNode,
     VertexType,
     WeilerAtherton,
@@ -784,54 +784,8 @@ gpcas.geometry.Clip = Clip
 // endregion Clip
 
 // region TopPolygonNode
-gpcas.geometry.TopPolygonNode = function () {
-  this.top_node
-}
-gpcas.geometry.TopPolygonNode.prototype.add_local_min = function (x, y) {
-  var existing_min = this.top_node
-  this.top_node = new PolygonNode(existing_min, x, y)
-  return this.top_node
-}
-gpcas.geometry.TopPolygonNode.prototype.merge_left = function (p, q) {
-  /* Label contour as a hole */
-  q.proxy.hole = true
-  var top_node = this.top_node
+gpcas.geometry.TopPolygonNode = TopPolygonNode
 
-  if (p.proxy != q.proxy) {
-    /* Assign p's vertex list to the left end of q's list */
-    p.proxy.v[Clip.RIGHT].next = q.proxy.v[Clip.LEFT]
-    q.proxy.v[Clip.LEFT] = p.proxy.v[Clip.LEFT]
-
-    /* Redirect any p.proxy references to q.proxy */
-    var target = p.proxy
-    for (var node = top_node; node != null; node = node.next) {
-      if (node.proxy == target) {
-        node.active = 0
-        node.proxy = q.proxy
-      }
-    }
-  }
-}
-gpcas.geometry.TopPolygonNode.prototype.merge_right = function (p, q) {
-  var top_node = this.top_node
-  /* Label contour as external */
-  q.proxy.hole = false
-
-  if (p.proxy != q.proxy) {
-    /* Assign p's vertex list to the right end of q's list */
-    q.proxy.v[Clip.RIGHT].next = p.proxy.v[Clip.LEFT]
-    q.proxy.v[Clip.RIGHT] = p.proxy.v[Clip.RIGHT]
-
-    /* Redirect any p->proxy references to q->proxy */
-    var target = p.proxy
-    for (var node = top_node; node != null; node = node.next) {
-      if (node.proxy == target) {
-        node.active = 0
-        node.proxy = q.proxy
-      }
-    }
-  }
-}
 gpcas.geometry.TopPolygonNode.prototype.count_contours = function () {
   var nc = 0
 
@@ -861,6 +815,7 @@ gpcas.geometry.TopPolygonNode.prototype.count_contours = function () {
   }
   return nc
 }
+
 gpcas.geometry.TopPolygonNode.prototype.getResult = function (polyClass) {
   var top_node = this.top_node
   var result = Clip.createNewPoly(polyClass)
@@ -920,27 +875,4 @@ gpcas.geometry.TopPolygonNode.prototype.getResult = function (polyClass) {
   }
   return result
 }
-gpcas.geometry.TopPolygonNode.prototype.print = function () {
-  //console.log("---- out_poly ----");
-  var top_node = this.top_node
-  var c = 0
-  var npoly_node = null
-  for (var poly_node = top_node; poly_node != null; poly_node = npoly_node) {
-    //console.log("contour="+c+"  active="+poly_node.active+"  hole="+poly_node.proxy.hole);
-    npoly_node = poly_node.next
-    if (poly_node.active != 0) {
-      var v = 0
-      for (
-        var vtx = poly_node.proxy.v[Clip.LEFT];
-        vtx != null;
-        vtx = vtx.next
-      ) {
-        //console.log("v="+v+"  vtx.x="+vtx.x+"  vtx.y="+vtx.y);
-      }
-      c++
-    }
-  }
-}
 // endregion TopPolygonNode
-
-const { TopPolygonNode } = gpcas.geometry
